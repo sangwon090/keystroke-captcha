f6a7bb9191b1c385f39693815771a6118b16969a
Sangwon Ryu <bdboy090@naver.com>
2021-06-20 23:14:48 +0900

    SvelteKit 업데이트

--- keystroke-collector/svelte.config.js
+++ keystroke-collector/svelte.config.js
@@ -1,13 +1,15 @@
-const preprocess = require('svelte-preprocess');
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
-module.exports = {
+const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
+		target: '#svelte'
 	}
 };
+
+export default config;
